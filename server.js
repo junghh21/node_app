395c6b699355744566918c36ae6b27c5f24edb78
junghh21 <junghh21@gmail.com>
2025-04-05 18:27:31 +0900

    fs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // Define a route
-app.get("/", (req, res) => {
+app.get("/aa", (req, res) => {
   res.send("Welcome to your Express server!");
 });
 
