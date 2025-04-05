b7816ddde3e04a91146f4bcf0532a42d63eba095
junghh21 <junghh21@gmail.com>
2025-04-05 18:22:05 +0900

    sdf

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const { JSONRPCServer } = require("json-rpc-2.0");
-
-const server = new JSONRPCServer();
-
-// Add methods to the server
-server.addMethod("echo", ({ text }) => text);
-server.addMethod("add", ({ a, b }) => a + b);
-
 const app = express();
-app.use(bodyParser.json());
 
-app.post("/json-rpc", (req, res) => {
-  const jsonRPCRequest = req.body;
-
-  server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
-    if (jsonRPCResponse) {
-      res.json(jsonRPCResponse);
-    } else {
-      res.sendStatus(204); // No content for notifications
-    }
-  });
+// Define a route
+app.get("/", (req, res) => {
+  res.send("Welcome to your Express server!");
 });
 
-app.listen(3000, () => console.log("JSON-RPC server is running on port 3000"));
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
